5d7e3fcebdf716bbe929c831f028c4f4f70bfd24
roynald <roynald@gradin.co.id>
2022-02-23 19:30:46 +0700

    add Theme Variable

---
+++ lib/shared/theme.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color
+Color purpleColor = Color(0xff5C40CC);
+Color blackColor = Color(0xff1F1449);
+Color grayColor = Color(0xff9698A9);
+Color greenColor = Color(0xff0EC3AE);
+Color redColor = Color(0xffEB70A5);
+Color backgroundColor = Color(0xffFAFAFA);
+Color inactiveColor = Color(0xffDBD7EC);
+
+// Textstyle
+TextStyle blackFonts = GoogleFonts.poppins(
+  color: blackColor,
+);
+
+TextStyle purpleFonts = GoogleFonts.poppins(
+  color: purpleColor,
+);
+
+TextStyle grayFonts = GoogleFonts.poppins(
+  color: grayColor,
+);
+
+TextStyle redFonts = GoogleFonts.poppins(
+  color: redColor,
+);
+
+TextStyle whiteFonts = GoogleFonts.poppins(
+  color: Colors.white,
+);
+
+TextStyle greenFonts = GoogleFonts.poppins(
+  color: greenColor,
+);
+
+// font weight
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
